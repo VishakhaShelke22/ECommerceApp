0a3ed51dccf2f2c7c8f55892b88c82bde8b2ddec
VishakhaShelke22 <105160765+VishakhaShelke22@users.noreply.github.com>
2022-07-20 14:41:42 +0530

    Add files via upload

---
+++ E-Commerce App/ProductDetailTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ProductDetailTableViewCell.swift
+//  E-Commerce App
+//
+//  Created by Mac on 22/05/22.
+//
+
+import UIKit
+
+class ProductDetailTableViewCell: UITableViewCell {
+    
+    // MARK: Outlets
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var ratingLable: UILabel!
+    
+ 
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+       
+        
+    }
+    
+
+
+   
+
+}
+
+
+
+
+
+/* override func setSelected(_ selected: Bool, animated: Bool) {
+     super.setSelected(selected, animated: animated)
+
+     // Configure the view for the selected state
+ }*/
